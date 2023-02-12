dbec6d16ea3c39a4b8044a76b395e449743fd39e
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-02-11 23:58:26 -0300

    localStorage

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const palavras = [
     "abacate", "bola", "cachorro", "dado", "elefante","faca", "guitarra", "helicóptero", "internet", "jardim", "kangaroo", "leão", "macaco", "navio", "óculos", "piano", "quarto", "relógio", "sino", "tigre", "uva", "voo", "xícara", "zebra", "água", "banana", "casa", "dinheiro", "elefante", "fada", "girafa", "helicóptero", "igreja", "janela", "lebre", "maçã", "ninho", "óleo", "pera", "quadro", "rato", "sapato", "Naruto", "torre", "urso", "vaca", "xadrez", "árvore", "bebê", "cavalo", "dragão", "estrela", "foguete", "gelo", "hidrante", "ijuí", "jabuti", "lapis", "mesa", "navio", "ouro", "pássaro", "quebra-cabeça", "rã", "sol", "tambor", "unicórnio", "vírus", "xarope", "água-viva", "banheiro", "cachorro-quente", "dado", "elefante", "flor", "galinha", "hortelã", "igreja", "jardim", "laranja", "melancia", "novembro", "orquídea", "pato", "queijo", "rato", "sorvete", "teclado", "ursinho", "vassoura", "xícara", "yogurte", "zebra"
   ];  
 
-  localStorage.setItem("palavras", JSON.stringify(palavras));
+localStorage.setItem("palavras", JSON.stringify(palavras));
 
 
 const adicionaPalavraNoArray = ()=>  {
@@ -10,6 +10,7 @@ const adicionaPalavraNoArray = ()=>  {
     const novaPalavra = elemento.value    
         if(!palavras.includes(novaPalavra)) {
             palavras.push(novaPalavra)
+            localStorage.setItem("palavras", JSON.stringify(palavras));
             elemento.value = ''
             elemento.focus
             window.alert(palavras)         
