84878035dc81368150b16a4ef5b71e2d49318cf5
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-02-13 15:48:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 //     "abacate", "bola", "cachorro", "dado", "elefante","faca", "guitarra", "helicóptero", "internet", "jardim", "kangaroo", "leão", "macaco", "navio", "óculos", "piano", "quarto", "relógio", "sino", "tigre", "uva", "voo", "xícara", "zebra", "água", "banana", "casa", "dinheiro", "elefante", "fada", "girafa", "helicóptero", "igreja", "janela", "lebre", "maçã", "ninho", "óleo", "pera", "quadro", "rato", "sapato", "Naruto", "torre", "urso", "vaca", "xadrez", "árvore", "bebê", "cavalo", "dragão", "estrela", "foguete", "gelo", "hidrante", "ijuí", "jabuti", "lapis", "mesa", "navio", "ouro", "pássaro", "quebra-cabeça", "rã", "sol", "tambor", "unicórnio", "vírus", "xarope", "água-viva", "banheiro", "cachorro-quente", "dado", "elefante", "flor", "galinha", "hortelã", "igreja", "jardim", "laranja", "melancia", "novembro", "orquídea", "pato", "queijo", "rato", "sorvete", "teclado", "ursinho", "vassoura", "xícara", "yogurte", "zebra"
 //   ];  
   
-  const palavras = [
-    "abacate", "bola", "cachorro", "dado", "elefante","faca"
-  ]; 
+//   const palavras = [
+//     "abacate", "bola", "cachorro", "dado", "elefante","faca"
+//   ]; 
 
 // const palavras = [];
 
+palavras = JSON.parse(localStorage.getItem("palavras"));
 localStorage.setItem("palavras", JSON.stringify(palavras));
 
 
